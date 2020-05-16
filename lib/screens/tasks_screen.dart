6309855a5f5c45429d58675be9a8939a15b4030c
Mohammed Samy <mosamy19@gmail.com>
2020-05-16 03:51:46 +0200

    Bottom sheet widget

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todoeyflutter/widgets/tasks_list.dart';
+import 'add_task_screen.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
@@ -72,7 +73,10 @@ class TasksScreen extends StatelessWidget {
         child: Icon(
           Icons.add,
         ),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (context) => AddTaskScreen());
+        },
       ),
     );
   }
